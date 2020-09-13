2b3e6e6b2de508f28c5db21c8aeda2b089a384e0
JoelPagliuca <JoelPagliuca@users.noreply.github.com>
2020-09-13 21:02:42 +1000

    initial spring project

---
+++ src/test/kotlin/me/joelpagliuca/trashbet/TrashbetApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.joelpagliuca.trashbet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrashbetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
