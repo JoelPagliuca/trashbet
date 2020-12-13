cb78d9facf9427aaa0352620bd0f2d87c9169d3a
JoelPagliuca <JoelPagliuca@users.noreply.github.com>
2020-12-13 22:45:38 +1100

    add ibm carbon

--- app/web/src/App.svelte
+++ app/web/src/App.svelte
@@ -1,30 +1,45 @@
 <script>
-	export let name;
+  import {
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    HeaderUtilities,
+    SkipToContent,
+    Content,
+    Grid,
+    Row,
+    Column,
+    HeaderGlobalAction,
+  } from "carbon-components-svelte";
+  import UserAvatarFilledAlt from "carbon-icons-svelte/lib/UserAvatarFilledAlt20";
+  import Theme from "./components/Theme.svelte";
+
+  let isSideNavOpen = false;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Theme theme="g90">
+  <Header company="" platformName="Trashbet" bind:isSideNavOpen>
+    <div slot="skip-to-content">
+      <SkipToContent />
+    </div>
+    
+    <HeaderNav>
+      <HeaderNavItem href="/" text="Home" />
+    </HeaderNav>
+    
+    <HeaderUtilities>
+      <HeaderGlobalAction aria-label="Login" icon={UserAvatarFilledAlt}/>
+    </HeaderUtilities>
+  </Header>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  <Content>
+    <Grid>
+      <Row>
+        <Column>
+          <h1>Trashbet</h1>
+        </Column>
+      </Row>
+    </Grid>
+  </Content>
+</Theme>
