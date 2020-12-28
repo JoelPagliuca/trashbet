f0d632cb04a7421e3a7193871c01406ffb83a399
JoelPagliuca <JoelPagliuca@users.noreply.github.com>
2020-12-28 21:57:13 +1100

    nicer login UX

--- app/web/src/components/Login.svelte
+++ app/web/src/components/Login.svelte
@@ -1,13 +1,13 @@
 <script>
-  import { Button, Form, FormGroup, TextInput } from "carbon-components-svelte";
+  import { Button, Form, FormGroup, InlineLoading, TextInput } from "carbon-components-svelte";
   import { store } from "../auth.js";
-  import { apiEndpoint } from "../environment.js";
   import { apiFetch } from "../api.js";
   let username = ""
   let password = ""
-  function handleSubmit() {
-    console.log("Login.handleSubmit()")
-    apiFetch("/login", {
+  let loginPromise = null
+
+  async function login() {
+    const res = await apiFetch("/login", {
       method: "POST",
       body: JSON.stringify({
         username: username,
@@ -15,16 +15,30 @@
       }),
       credentials: "omit",
     })
+    if (res.ok) {
+      $store = { "username": username }
+      return
+    } else {
+      throw new Error("bad login")
+    }
+  }
+
+  function handleSubmit() {
+    loginPromise = login()
   }
 </script>
 <div>
   <h2>Login</h2>
-  <pre>{JSON.stringify($store, null, 2)}</pre>
   <Form on:submit="{handleSubmit}">
     <FormGroup>
       <TextInput labelText="Username" type="text" bind:value={username} />
       <TextInput labelText="Password" type="password" bind:value={password} />
     </FormGroup>
     <Button type="submit">Login</Button>
+    {#await loginPromise}
+      <InlineLoading status="active" description="loading..." />
+    {:catch error}
+      <InlineLoading status="error" description={error} />
+    {/await}
   </Form>
 </div>
